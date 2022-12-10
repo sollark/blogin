00743ae056c37dbc737e184a6d231e7dad3e5fc1
Andrey Solonikov <andreysol@yahoo.com>
2022-12-10 09:46:35 +0200

    some fixes

--- js/services/post.service.js
+++ js/services/post.service.js
@@ -59,18 +59,3 @@ function _createPosts() {
 function _savePostsToStorage() {
   saveToLS(STORAGE_KEY, gPosts);
 }
-
-/* 
-
-// in utils.js
-
-function makeId(length = 6) {
-  const possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var txt = '';
-  for (var i = 0; i < length; i++) {
-    txt += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return txt;
-}
- */
