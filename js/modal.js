d811c1a7bab1998584f1d3b02346a8ca88648625
Andrey Solonikov <andreysol@yahoo.com>
2022-12-11 21:36:47 +0200

    fixes, added modal

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const elModal = document.querySelector('.modal');
+const elText = document.querySelector('.modal__text');
+
+function openModal() {
+  if (typeof elModal.showModal === 'function') {
+    elModal.showModal();
+
+    // disable scrolling
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100%';
+  }
+}
+
+// close buttons
+const elCloseIcon = document.querySelector('.modal__closeIcon');
+elCloseIcon.addEventListener('click', closeModal);
+
+function closeModal() {
+  elModal.close(this);
+
+  // enable scrolling
+  document.body.style.overflow = 'auto';
+  document.body.style.height = 'auto';
+}
+
+// when modal closed, it send here form submit values
+elModal.addEventListener('close', () => {
+  console.log('Submitted value: "' + elModal.returnValue + '"');
+
+  const email = elModal.querySelector('input').value;
+  console.log('elFormInputs:', email);
+});
--- js/post.controller.js
+++ js/post.controller.js
@@ -12,21 +12,25 @@ function renderPosts() {
   const strHTMLs = posts.map(
     (post) => `
         <section class="card">
-            <img class="card-image" src="assets/img/${post.imgUrl}-thumbnail.png" alt="Image" />
-            <article class="card-article">
+            <img class="card-image " src="assets/img/${post.imgUrl}-thumbnail.png" alt="Image" />
+            <article class="card-article flex flex-column">
                 <h1 class="card-title">
                     ${post.title}
                 </h1>
                 <p>
                     ${post.text}
                 </p>
-                <button class="card-button">Continue Reading</button>
+                <button class="card-button btn--style" onclick="onPostBtn()">Continue Reading</button>
             </article>
         </section>
       `
   );
 
   document.querySelector('.main-content').innerHTML = strHTMLs.join('');
+}
+
+function onPostBtn() {
+  openModal();
 }
 
 function onAddPost(ev) {
